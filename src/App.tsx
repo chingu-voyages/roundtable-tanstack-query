c6e3f8905d5c321805f2c50b32cb76db333953ce
Andres Court <andres@andrescourt.com>
2023-11-20 20:19:31 -0500

    Read data from the api and display it in the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
 import { useEffect, useState } from "react"
-import todoItems from "../data/todos.json"
 import TodoList from "./components/todolist"
 import { TodoType } from "./types/type"
+import { getInfo } from "./utils/getInfo"
 
 function App() {
      const [todos, setTodos] = useState<TodoType[]>([])
      const [loading, setLoading] = useState(false)
+     const [isError, setIsError] = useState(false)
+     const [errorMessage, setErrorMessage] = useState("")
 
      useEffect(() => {
           setLoading(true)
-          setTodos(todoItems)
-          const timeout = setTimeout(() => {
-               setLoading(false)
-          }, 1000)
+          setIsError(false)
 
-          return () => {
-               clearTimeout(timeout)
-          }
+          getInfo().then((data) => {
+               setTodos(data)
+          }).catch((err) => {
+               setIsError(true)
+               setErrorMessage(err.message)
+          })
 
+          setLoading(false)
      }, [todos])
 
      if (loading) {
-          return <div>Loading...</div>
+          return (
+               <div className="bg-gradient-to-br from-purple-500 to-purple-900 min-h-screen">
+                    <div className="pt-5 text-slate-100 text-center  uppercase fontextrabold">Loading...</div>
+               </div>
+          )
      }
 
+     if (isError) {
+          return (
+               <div className="bg-gradient-to-br from-purple-500 to-purple-900 min-h-screen">
+                    <div className="pt-5 text-slate-100 text-center fontextrabold">Error: {errorMessage}</div>
+               </div>
+          )
+     }
 
      return (
           <div className="bg-gradient-to-br from-purple-500 to-purple-900 min-h-screen">
