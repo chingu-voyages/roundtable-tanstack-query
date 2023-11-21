79bfd178fa52f018d782168f0c474688d4ae5d31
Andres Court <andres@andrescourt.com>
2023-11-20 20:27:54 -0500

    added a query with errors for testing

--- src/utils/getInfo.ts
+++ src/utils/getInfo.ts
@@ -3,5 +3,11 @@ import { TodoType } from "../types/type";
 
 export async function getInfo(): Promise<TodoType[]> {
      const response = await axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
-     return response.data as TodoType[]
+     return response.data
+}
+
+
+export async function getErrorInfo(): Promise<TodoType[]> {
+     const response = await axios.get("https://jsonplaceholder.typicode.com/todo?_limit=10")
+     return response.data
 }
